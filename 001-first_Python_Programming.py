c405d31844bc18fc9ba3a7a85dae1bd4bc8900eb
EMMA <69660913+Emma2410@users.noreply.github.com>
2021-08-09 14:16:05 +0630

    Add files via upload

---
+++ 001-first_Python_Programming.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 5
+type(x)
+
+
+# In[2]:
+
+
+x = 3.5 
+type(x)
+
+
+# In[3]:
+
+
+x = "Hello"
+type(x)
+
+
+# In[9]:
+
+
+x==y
+
+
+# In[10]:
+
+
+print(x)
+
+
+# In[11]:
+
+
+x>y
+
+
+# In[12]:
+
+
+x<y
+
+
+# In[13]:
+
+
+x!=y
+
+
+# In[14]:
+
+
+x>=y
+
+
+# In[15]:
+
+
+x<=y
+
+
+# In[16]:
+
+
+a= True
+b= False
+print(a and b)
+
+
+# In[17]:
+
+
+print(a or b)
+
+
+# In[18]:
+
+
+print(not(a))
+
+
+# In[19]:
+
+
+print(a or b)
+
+
+# In[20]:
+
+
+text = "I am a string"
+print(text)
+
+
+# In[22]:
+
+
+text = 'I\'m a string' 
+print(text)
+
+
+# In[23]:
+
+
+print('myanmar'+ 'data science')
+
+
+# In[24]:
+
+
+print("Myanmar" * 3)
+
+
+# In[ ]:
+
+
+
+
